d7f7f38472e9c27ed606e8469c3bee54d2b74e37
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-12 20:52:55 +0300

    Replace Batik::Document module with class

--- lib/batik/document.rb
+++ lib/batik/document.rb
@@ -3,67 +3,67 @@ module Batik
   import org.apache.batik.dom.util.DOMUtilities
   import java.io.StringWriter
 
-  module Document
-    def self.included(base)
-      base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
+  class Document
+    attr_reader :elements
 
-      base.initialize_svg_namespace
+    def initialize(options = {}, &block)
+      @root_attributes = options
+      @elements = []
+
+      instance_exec(&block) if block_given?
     end
 
-    module InstanceMethods
-      def to_svg
-        writer = StringWriter.new
+    def to_svg
+      document = create_svg_document
+      set_root_attributes(document)
 
-        DOMUtilities.writeDocument self.class.svg_document, writer
+      writer = StringWriter.new
 
-        writer.to_s
+      DOMUtilities.writeDocument document, writer
+
+      writer.to_s
+    end
+
+    def create_svg_document
+      dom = SVGDOMImplementation.getDOMImplementation
+      dom.createDocument SVGDOMImplementation::SVG_NAMESPACE_URI, "svg", nil
+    end
+
+    def set_root_attributes(document)
+      root = document.getDocumentElement
+      @root_attributes.each do |key, value|
+        root.setAttributeNS nil, key.to_s, value.to_s
       end
     end
 
-    module ClassMethods
-      def svg_document
-        @svg_document
-      end
-
-      def svg_document=(doc)
-        @svg_document = doc
-      end
-
-      def root
-        svg_document.getDocumentElement
-      end
-
-      def initialize_svg_namespace
-        dom = SVGDOMImplementation.getDOMImplementation
-        self.svg_document = dom.createDocument SVGDOMImplementation::SVG_NAMESPACE_URI, "svg", nil
-      end
-
-      def width(value)
-        root.setAttributeNS nil, 'width', value.to_s
-      end
-
-      def height(value)
-        root.setAttributeNS nil, 'height', value.to_s
-      end
-
-      def rectangle(options)
-        element = svg_document.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, 'rect')
-        options.each do |key, value|
-          element.setAttributeNS nil, key.to_s, value.to_s
-        end
-
-        root.appendChild element
-      end
-
-      def circle(options)
-        element = svg_document.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, 'circle')
-        options.each do |key, value|
-          element.setAttributeNS nil, key.to_s, value.to_s
-        end
-
-        root.appendChild element
-      end
-    end
+#    def root
+#      svg_document.getDocumentElement
+#    end
+#
+#    def width(value)
+#      root.setAttributeNS nil, 'width', value.to_s
+#    end
+#
+#    def height(value)
+#      root.setAttributeNS nil, 'height', value.to_s
+#    end
+#
+#    def rectangle(options)
+#      element = svg_document.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, 'rect')
+#      options.each do |key, value|
+#        element.setAttributeNS nil, key.to_s, value.to_s
+#      end
+#
+#      root.appendChild element
+#    end
+#
+#    def circle(options)
+#      element = svg_document.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, 'circle')
+#      options.each do |key, value|
+#        element.setAttributeNS nil, key.to_s, value.to_s
+#      end
+#
+#      root.appendChild element
+#    end
   end
 end
--- spec/batik/document_spec.rb
+++ spec/batik/document_spec.rb
@@ -1,61 +1,81 @@
 require 'spec_helper'
 
 describe Batik::Document do
-  class Empty
-    include Batik::Document
-  end
+  context 'when created' do
+    it 'accept options' do
+      Batik::Document.new(:width => 500)
+    end
 
-  context 'when included' do
-    it 'provide the minimum svg representation' do
-      Empty.new.to_svg.should eq '<svg contentScriptType="text/ecmascript" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"/>'
+    it 'accept optional block that is evaluated in the new object context' do
+      Batik::Document.new(:width => 500) do
+        elements
+      end
     end
   end
 
-  describe '.width' do
-    class Width
-      include Batik::Document
+  context 'when empty' do
+    let(:svg) { Batik::Document.new }
 
-      width 400
-    end
-
-    it "set the outer svg element's width" do
-      Width.new.to_svg.should match /width="400"/
+    it 'render to empty svg representation' do
+      svg.to_svg.should eq '<svg contentScriptType="text/ecmascript" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"/>'
     end
   end
 
-  describe '.height' do
-    class Height
-      include Batik::Document
+  context 'when empty, but with options' do
+    let(:svg) { Batik::Document.new(:width => 500, :height => 200) }
 
-      height 300
-    end
-
-    it "set the outer svg element's height" do
-      Height.new.to_svg.should match /height="300"/
+    it 'render to empty svg root tag with given options set as attributes' do
+      svg.to_svg.should match 'width="500"'
+      svg.to_svg.should match 'height="200"'
     end
   end
 
-  describe '.rectangle' do
-    class Rectangle
-      include Batik::Document
-
-      rectangle :x => 10, :y => 20, :fill => 'red', :width => 20, :height => 30
-    end
-
-    it 'creates new rectangle object with the given options' do
-      Rectangle.new.to_svg.should match /<rect width="20" x="10" fill="red" height="30" y="20"\/>/
-    end
-  end
-
-  describe '.circle' do
-    class Circle
-      include Batik::Document
-
-      circle :cx => 10, :cy => 10, :r => 20, :fill => 'blue'
-    end
-
-    it 'creates new circle object with the given options' do
-      Circle.new.to_svg.should match /<circle fill="blue" r="20" cx="10" cy="10"\/>/
-    end
-  end
+#
+#  describe '.width' do
+#    class Width
+#      include Batik::Document
+#
+#      width 400
+#    end
+#
+#    it "set the outer svg element's width" do
+#      Width.new.to_svg.should match /width="400"/
+#    end
+#  end
+#
+#  describe '.height' do
+#    class Height
+#      include Batik::Document
+#
+#      height 300
+#    end
+#
+#    it "set the outer svg element's height" do
+#      Height.new.to_svg.should match /height="300"/
+#    end
+#  end
+#
+#  describe '.rectangle' do
+#    class Rectangle
+#      include Batik::Document
+#
+#      rectangle :x => 10, :y => 20, :fill => 'red', :width => 20, :height => 30
+#    end
+#
+#    it 'creates new rectangle object with the given options' do
+#      Rectangle.new.to_svg.should match /<rect width="20" x="10" fill="red" height="30" y="20"\/>/
+#    end
+#  end
+#
+#  describe '.circle' do
+#    class Circle
+#      include Batik::Document
+#
+#      circle :cx => 10, :cy => 10, :r => 20, :fill => 'blue'
+#    end
+#
+#    it 'creates new circle object with the given options' do
+#      Circle.new.to_svg.should match /<circle fill="blue" r="20" cx="10" cy="10"\/>/
+#    end
+#  end
 end
