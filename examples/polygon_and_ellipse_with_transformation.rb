234023e8cf9dd862650e8afa7f95bb55c9c81eba
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-14 23:07:40 +0300

    Add polygon and ellipse example

---
+++ examples/polygon_and_ellipse_with_transformation.rb
@@ -0,0 +1,10 @@
+require 'batik'
+
+svg = Batik::SVG.new(width: '1200', height: '400') do
+  polygon :fill => 'red', :points => '850,75  958,137.5 958,262.5 850,325 742,262.6 742,137.5'
+  ellipse :transform => 'translate(900 200) rotate(-30)', :rx => 250, :ry => 100, :fill => 'none', :stroke => 'blue', 'stroke-width' => 20
+end
+
+File.open('polygon_and_ellipse_with_transformation.svg', 'w+') do |file|
+  file << svg.to_s
+end
