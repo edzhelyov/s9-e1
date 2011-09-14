ec25725f049893d0bc87b0251ce4fc9529ac91e7
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-14 22:31:25 +0300

    Use require_relative

--- lib/batik.rb
+++ lib/batik.rb
@@ -1,13 +1,13 @@
 require 'java'
 require_relative '../vendor/batik-1.7/batik.jar'
 
-require 'batik/attribute_methods'
-require 'batik/document'
-require 'batik/element'
-require 'batik/rectangle'
-require 'batik/circle'
-require 'batik/text'
-require 'batik/svg'
+require_relative 'batik/attribute_methods'
+require_relative 'batik/document'
+require_relative 'batik/element'
+require_relative 'batik/rectangle'
+require_relative 'batik/circle'
+require_relative 'batik/text'
+require_relative 'batik/svg'
 
 module Batik
   import org.apache.batik
