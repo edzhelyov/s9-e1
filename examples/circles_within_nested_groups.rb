3254b565bbf0286e67940e595393f09f129e7bfd
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-18 23:33:07 +0300

    Example use the new 'group' method instead of 'g'

--- examples/circles_within_nested_groups.rb
+++ examples/circles_within_nested_groups.rb
@@ -1,11 +1,11 @@
 require 'batik'
 
 svg = Batik::SVG.new(width: 400, height: 400) do
-  g :fill => 'red' do
+  group :fill => 'red' do
     circle :cx => 100, :cy => 100, :r => 50
-    g :stroke => 'black', 'stroke-width' => 15 do
+    group :stroke => 'black', 'stroke-width' => 15 do
       circle :cx => 200, :cy => 200, :r => 30
-      g :fill => 'blue' do
+      group :fill => 'blue' do
         circle :cx => 300, :cy => 300, :r => 40
       end
     end
