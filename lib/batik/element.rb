b509af0145a186989d1c4428f2dbaf786b475932
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-19 17:27:27 +0300

    Simplified the metaprogramming in Element module

--- lib/batik/element.rb
+++ lib/batik/element.rb
@@ -1,42 +1,49 @@
 module Batik
   module Element
     def self.included(base)
-      base.class_eval <<-EOS
-        attr_reader :attributes
-
-        def self.set_type(name)
-          @type = name
-        end
-
-        def self.type
-          @type
-        end
-
-        def self.register(method_name) 
-          Batik::SVGElements.module_eval <<-EOF
-            def \#{method_name}(attributes)
-              @elements << \#{name}.new(attributes)
-            end
-          EOF
-        end
-      EOS
+      base.module_eval do
+        extend ClassMethods
+        include InstanceMethods
+      end
     end
 
-    include AttributeMethods
+    module ClassMethods
+      def set_type(name)
+        @type = name
+      end
 
-    def initialize(attributes = {})
-      @attributes = attributes
+      def type
+        @type
+      end
+
+      def register(method_name)
+        Batik::SVGElements.module_eval <<-EOF
+          def #{method_name}(attributes)
+            @elements << #{name}.new(attributes)
+          end
+        EOF
+      end
     end
 
-    def to_batik_element(doc)
-      batik_element = doc.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, type)
-      set_attributes(batik_element, attributes)
+    module InstanceMethods
+      attr_reader :attributes
 
-      batik_element
-    end
+      include AttributeMethods
 
-    def type
-      self.class.type
+      def initialize(attributes = {})
+        @attributes = attributes
+      end
+
+      def to_batik_element(doc)
+        batik_element = doc.createElementNS(SVGDOMImplementation::SVG_NAMESPACE_URI, type)
+        set_attributes(batik_element, attributes)
+
+        batik_element
+      end
+
+      def type
+        self.class.type
+      end
     end
   end
 end
