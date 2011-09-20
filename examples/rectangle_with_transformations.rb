adf11e21a8ed6347e71851f5e641e152684715a8
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-20 23:30:05 +0300

    Add example with block syntax and transformations

---
+++ examples/rectangle_with_transformations.rb
@@ -0,0 +1,14 @@
+require 'batik'
+
+Batik::SVG.draw('rectangle_with_transofrmations.svg', width: 400, height: 400) do
+  group do
+    translate 200, 200
+    rotate 30
+    scale 2, 5
+    
+    rectangle do
+      coordinates 0, 0
+      dimensions 50, 30
+    end
+  end
+end
