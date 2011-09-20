e826f8f8fe95f36330d0c724aa2ef9956af9b372
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-20 18:16:04 +0300

    Add optional empty {} when calling element methods in the DSL

--- lib/batik/svg_elements.rb
+++ lib/batik/svg_elements.rb
@@ -2,13 +2,13 @@ module Batik
   module SVGElements
     def self.register(method_name, klass)
       define_method method_name.to_sym do |attributes|
-        @elements << klass.new(attributes)
+        @elements << klass.new(attributes || {})
       end
     end
 
     def self.register_with_block(method_name, klass)
       define_method method_name.to_sym do |attributes, &block|
-        @elements << klass.new(attributes, &block)
+        @elements << klass.new(attributes || {}, &block)
       end
     end
   end
