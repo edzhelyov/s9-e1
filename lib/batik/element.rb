576262ac531dbdb550ae022a6056712812af0d44
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-21 22:22:12 +0300

    Use ordinary class idioms instead of module ones in Element

--- lib/batik/element.rb
+++ lib/batik/element.rb
@@ -1,7 +1,12 @@
 module Batik
   class Element
+    attr_reader :attributes
 
-    module ClassMethods
+    include AttributeMethods
+    include ElementAttributes::Color
+    include ElementAttributes::Transformations
+
+    class << self
       def set_type(name)
         @type = name
       end
@@ -15,33 +20,22 @@ module Batik
       end
     end
 
-    module InstanceMethods
-      attr_reader :attributes
+    def initialize(attributes = {}, &block)
+      @attributes = attributes
 
-      include AttributeMethods
-      include ElementAttributes::Color
-      include ElementAttributes::Transformations
-
-      def initialize(attributes = {}, &block)
-        @attributes = attributes
-
-        instance_exec(&block) if block_given?
-      end
-
-      def to_batik_element(doc)
-        namespace = SVGDOMImplementation::SVG_NAMESPACE_URI
-        batik_element = doc.createElementNS(namespace, type)
-        set_attributes(batik_element, attributes)
-
-        batik_element
-      end
-
-      def type
-        self.class.type
-      end
+      instance_exec(&block) if block_given?
     end
 
-    extend ClassMethods
-    include InstanceMethods
+    def to_batik_element(doc)
+      namespace = SVGDOMImplementation::SVG_NAMESPACE_URI
+      batik_element = doc.createElementNS(namespace, type)
+      set_attributes(batik_element, attributes)
+
+      batik_element
+    end
+
+    def type
+      self.class.type
+    end
   end
 end
